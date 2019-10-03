705e9b312413fa9f5bf55a6ccee2f40083c6673b
Anjali Choudhary <anjalichoudhary1999.ac@gmail.com>
2019-10-03 15:46:18 +0530

    Added Timer Move-counter

--- js/app.js
+++ js/app.js
@@ -4,13 +4,13 @@
 var arr = document.getElementsByClassName('card');
 
 let noofmoves = 0 ;
-let noofopen = 14 ;
+let noofopen = 0 ;
 var lastopen;
-var s =0;
-var m =0;
-flag = 0;
 const rest = document.querySelector('.restart');
 rest.addEventListener('click',restart_func);
+let hasstarted = false;
+
+var start_time,x;
 
 restart_func();
 
@@ -29,33 +29,28 @@ function shuffle(array) {
     return array;
 }
 
+function starter()
+{
+    x = setInterval(function()
+    {
+    var now = new Date().getTime();
+    var distance = now - start_time;
+    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+    document.querySelector('.timer').textContent = minutes + "m " + seconds + "s";
+    document.querySelector('.fmins').textContent = minutes;
+    document.querySelector('.fsecs').textContent = seconds;
+    },1000);
+
+}
 
 function restart_func()
 {
      arr=shuffle(arr);
      noofmoves=0;
      document.querySelector('.moves').textContent = String(noofmoves);
-     document.querySelector('.mins').textContent = "00";
-     document.querySelector('.secs').textContent = "00";
-     s = 0;
-     m = 0;
-     flag = 0;
-}
-function incre()
-{
-    document.querySelector('.mins').textContent = String(getmins());
-    document.querySelector('.secs').textContent = String(getsecs());
-    s++;
-    setTimeout(incre(),1000);
-}
-function getmins()
-{
-    m = Math.floor(s/ 60); 
-    return m; 
-}
-function getsecs()
-{
-    return s - Math.round(m * 60);
+     document.querySelector('.timer').textContent = "0m 0s"
+     clearInterval(x);
 }
 let cls = document.getElementsByClassName('deck')[0];
 for(i = 0; i < 16; i++)
@@ -64,36 +59,35 @@ for(i = 0; i < 16; i++)
     let temp = arr[i];
     temp.addEventListener('click',function(){
         
-        /*if(flag === 0)
-        {
-            flag = 1;
-            setTimeout(incre());
-        }*/
         if(!temp.classList.contains("match") && !temp.classList.contains("show"))
         {
             noofmoves+=1;
-            if(noofmoves%2==0)
-            document.querySelector('.moves').textContent = String(noofmoves/2);
+            if(noofmoves==1)
+            {
+                start_time = new Date().getTime();
+                starter();
+            }
             temp.className = "card open show";
-            noofopen+=1;
             if(noofmoves%2==0)
             {
+                document.querySelector('.moves').textContent = String(noofmoves/2);
                 if(lastopen.childNodes[1].className !== temp.childNodes[1].className)
                 {
                     setTimeout(function(){
                     lastopen.className = "card";
                     temp.className = "card";
-                    noofopen-=2;
                     },500);
                 }
                 else
                 {
                     lastopen.className = "card match";
                     temp.className = "card match";
+                    noofopen+=2;
                 }
                 if(noofopen == 16)
                 {
                     game_end();
+                    clearInterval(x);
                 }
             }
             else
@@ -103,6 +97,7 @@ for(i = 0; i < 16; i++)
 }
 function game_end()
 {
+    console.log("hello");
     var modal = document.getElementById("myModal");
     var span = document.getElementsByClassName("close")[0];
     modal.style.display = "block";
